c40e05042f443481d2b9283e38209ef746a53ec8
Shoaib Ahmed <shabiraja994@gmail.com>
2023-06-22 01:19:31 +0500

    f

--- controller/user.js
+++ controller/user.js
@@ -108,7 +108,7 @@ exports.generateFile= async (req, res, next) => {
   }
   const content = fs.readFileSync(
     // path.join(process.cwd(), `${filename}.docx`),
-    path.join(process.cwd(), 'files', 'labs.dpcx'),
+    path.join(process.cwd(),  'labs.dpcx'),
     "binary"
 );
 
