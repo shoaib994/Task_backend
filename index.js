a31a85b1d16c7712038d9c5437fa745bd022876b
Shoaib Ahmed <shabiraja994@gmail.com>
2023-06-22 01:07:26 +0500

    file path in index file

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const app=express()
 
 const cors = require("cors");
 const PORT=4000
+const path = require("path");
 
 app.listen(PORT,()=>console.log(`server is working on ${PORT}`))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 const corsOptions = {
     origin: '*',
     'Access-Control-Allow-Origin': '*',
